6bc8c922c70f835f8df8e7c62b3b7a853635c14c
cemik <aveal.cemik@gmail.com>
2024-04-02 22:45:46 +0200

    логика чтение бд и отправка csv в бесконечном цикле

--- TaskMasterServer/TaskMasterServer/DataBase/Task.cs
+++ TaskMasterServer/TaskMasterServer/DataBase/Task.cs
@@ -6,30 +6,16 @@ namespace TaskMasterServer.DataBase;
 public partial class Task
 {
     public int TaskId { get; set; }
-
     public string? TaskName { get; set; }
-
     public string? Description { get; set; }
-
     public DateOnly? DateCreate { get; set; }
-
     public DateOnly? Deadline { get; set; }
-
     public int? StatusId { get; set; }
-
     public int? PriorityId { get; set; }
-
     public int? UserId { get; set; }
-
     public int? AttachmentId { get; set; }
-
     public virtual Attachment? Attachment { get; set; }
-
     public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
-
     public virtual Priority? Priority { get; set; }
-
     public virtual Status? Status { get; set; }
-
-    public virtual User? User { get; set; }
-}
+    public virtual User? User { get; set; }}
---
+++ TaskMasterServer/TaskMasterServer/TaskMasterServer.cs
@@ -0,0 +1,65 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using System.Net;
+using TaskMasterServer.DataBase;
+using TaskBD = TaskMasterServer.DataBase.Task;
+
+
+List<TaskBD> tempBD = new();
+
+using (TaskUserDbContext dbContext = new TaskUserDbContext())
+{
+    tempBD = dbContext.Tasks.ToList();
+}
+
+string local = "http://127.0.0.1:8888/GetTask/";
+
+HttpListener server = new HttpListener();
+server.Prefixes.Add(local);
+server.Start();
+Console.WriteLine("Сервер запущен");
+
+while (true)
+{
+    HttpListenerContext context = await server.GetContextAsync();
+    HttpListenerResponse response = context.Response;
+
+    string csvData;
+    using StringWriter writer = new StringWriter();
+    using (CsvWriter csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+    {
+        csvWriter.WriteRecords(tempBD);
+        csvData = writer.ToString();
+    }
+
+    byte[] buffer = System.Text.Encoding.Unicode.GetBytes(csvData);
+    response.ContentLength64 = buffer.Length;
+    Stream output = response.OutputStream;
+    output.Write(buffer, 0, buffer.Length);
+    output.Close();
+}
+
+
+//using (TaskUserDbContext contex = new TaskUserDbContext())
+//{
+//    tempBD = contex.Tasks.ToList();
+//}
+
+//var csvContext = new CsvContext(new CsvConfiguration(CultureInfo.InvariantCulture));
+
+
+//var request = context.Request;
+//var user = context.User;
+
+//byte[] buffer = Encoding.UTF8.GetBytes();
+//response.ContentLength64 = buffer.Length;
+//using Stream output = response.OutputStream;
+
+//await output.WriteAsync(buffer);
+//await output.FlushAsync();
+
+//Console.WriteLine("Запрос обработан");
+
+//server.Stop();
+//server.Close();
